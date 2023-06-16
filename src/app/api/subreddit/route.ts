8d85080de9113f9f346b54c70b6f631d037726cd
Sukrit Saha <sukritsaha27@gmail.com>
2023-06-16 23:11:35 +0530

    endpoints for creating subreddits and proper error handling

---
+++ src/app/api/subreddit/route.ts
@@ -0,0 +1,52 @@
+import { db } from "@/lib/db";
+import { getAuthSession } from "@/lib/auth";
+import { SubredditValidator } from "@/lib/validators/subreddit";
+import { z } from "zod";
+
+export async function POST(req: Request) {
+  try {
+    const session = await getAuthSession();
+
+    if (!session?.user) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    const body = await req.json();
+
+    const { name } = SubredditValidator.parse(body);
+
+    const existingSubreddit = await db.subreddit.findFirst({
+      where: {
+        name,
+      },
+    });
+
+    if (existingSubreddit) {
+      return new Response("Subreddit with this name already exists", {
+        status: 409,
+      });
+    }
+
+    const subreddit = await db.subreddit.create({
+      data: {
+        name,
+        creatorId: session.user.id,
+      },
+    });
+
+    await db.subscription.create({
+      data: {
+        userId: session.user.id,
+        subredditId: subreddit.id,
+      },
+    });
+
+    return new Response(subreddit.name);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response(error.message, { status: 422 });
+    }
+
+    return new Response("Something went wrong", { status: 500 });
+  }
+}
