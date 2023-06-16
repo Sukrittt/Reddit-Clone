67d073025a70e6ad828d8848143170b501bc1753
Sukrit Saha <sukritsaha27@gmail.com>
2023-06-16 18:01:06 +0530

    adding dropdowns and sign-out functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        {/* @ts-expect-error Server Component */}
         <Navbar />
         <div className="container max-w-7xl mx-auto h-full pt-12">
           {children}
