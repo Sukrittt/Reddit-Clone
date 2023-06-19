6c6fd727bd5627a4c9083a9bffbcb3b8c5afd656
Sukrit Saha <sukritsaha27@gmail.com>
2023-06-19 21:12:29 +0530

    search bar functionality

---
+++ src/app/api/search/route.ts
@@ -0,0 +1,28 @@
+import { db } from "@/lib/db";
+
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+  const q = url.searchParams.get("q");
+
+  if (!q) {
+    return new Response("Invalid Query", { status: 400 });
+  }
+
+  try {
+    const results = await db.subreddit.findMany({
+      where: {
+        name: {
+          startsWith: q,
+        },
+      },
+      include: {
+        _count: true,
+      },
+      take: 5,
+    });
+
+    return new Response(JSON.stringify(results));
+  } catch (error) {
+    return new Response("Something went wrong", { status: 500 });
+  }
+}
--- src/components/Navbar/SearchBar.tsx
+++ src/components/Navbar/SearchBar.tsx
@@ -1,9 +1,89 @@
-import { FC } from "react";
+"use client";
+import { FC, useCallback, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { Users } from "lucide-react";
+
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/ui/Command";
+import { Prisma, Subreddit } from "@prisma/client";
+import debounce from "lodash.debounce";
 
 interface SearchBarProps {}
 
 const SearchBar: FC<SearchBarProps> = ({}) => {
-  return <div>SearchBar</div>;
+  const [input, setInput] = useState<string>("");
+  const router = useRouter();
+
+  const {
+    data: queyrResults,
+    refetch,
+    isFetched,
+  } = useQuery({
+    queryFn: async () => {
+      if (!input) return [];
+
+      const { data } = await axios(`/api/search?q=${input}`);
+
+      return data as (Subreddit & {
+        _count: Prisma.SubredditCountOutputType;
+      })[];
+    },
+    queryKey: ["search-query"],
+    enabled: false, //by default it will not fetch
+  });
+
+  const request = debounce(() => {
+    refetch();
+  }, 300);
+
+  const debouncedRequest = useCallback(() => {
+    request();
+  }, []);
+
+  return (
+    <Command className="relative rounded-lg border max-w-lg z-50 overflow-visible">
+      <CommandInput
+        value={input}
+        onValueChange={(text) => {
+          setInput(text);
+          debouncedRequest();
+        }}
+        className="outline-none border-none focus:outline-none focus:border-none ring-0"
+        placeholder="Search communites..."
+      />
+
+      {input.length > 0 && (
+        <CommandList className="absolute bg-white top-full inset-x-0 shadow rounded-b-md">
+          {isFetched && <CommandEmpty>No results found.</CommandEmpty>}
+          {(queyrResults?.length ?? 0) > 0 && (
+            <CommandGroup heading="Communites">
+              {queyrResults?.map((subreddit) => (
+                <CommandItem
+                  key={subreddit.id}
+                  value={subreddit.name}
+                  onSelect={(e) => {
+                    router.push(`/r/${e}`);
+                    router.refresh();
+                  }}
+                >
+                  <Users className="mr-2 h-4 w-4" />
+                  <a href={`/r/${subreddit.name}`}>r/{subreddit.name}</a>
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          )}
+        </CommandList>
+      )}
+    </Command>
+  );
 };
 
 export default SearchBar;
