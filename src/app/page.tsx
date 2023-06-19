c50c7603ed226293324c82fca0bb00d02d6edf24
Sukrit Saha <sukritsaha27@gmail.com>
2023-06-19 23:04:28 +0530

    opting out of the caching behavior of next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { getAuthSession } from "@/lib/auth";
 import { CustomFeed, GeneralFeed } from "@/components/HomeFeed";
 
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
 export default async function Home() {
   const session = await getAuthSession();
